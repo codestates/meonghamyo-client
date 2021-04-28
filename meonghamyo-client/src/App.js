1f2dd362873fa2c983435e742ed8177439b03be2
codono <codonopsis0130@gmail.com>
2021-04-28 17:57:18 +0900

    [client]서덕주/feat/NavBar2

---
+++ meonghamyo-client/src/App.js
@@ -0,0 +1,50 @@
+// import './App.css';
+import './component/css/Nav.css'
+import React from 'react';
+// import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import ComunityList from './comunityList/ComunityList';
+import { AiOutlineMenu } from 'react-icons/ai';
+import CategoryModal from './component/modal/CategoryModal';
+import LoginModal from './component/modal/LoginModal'
+
+class App extends React.Component {
+  state = {
+    categoryModal: false,
+    loginModal: false
+  }
+  openCategoryModal = () => {
+    this.setState({ categoryModal: true });
+  }
+  closeCategoryModal = () => {
+    this.setState({ categoryModal: false });
+  }
+  openLoginModal = () => {
+    this.setState({ loginModal: true });
+  }
+  closeLoginModal = () => {
+    this.setState({ loginModal: false });
+  }
+  render() {
+    return (
+    <div className="App">
+      <nav>
+          <ul className='navContainer'>
+              <li id='ham' onClick={this.openCategoryModal}>
+                  <AiOutlineMenu />
+              </li>
+              <li id='navLogo'>MeongHaMyo</li>
+              <li id='loginBlock' onClick={this.openLoginModal}>
+                  로그인
+              </li>
+          </ul>
+      </nav>
+      <CategoryModal open={this.state.categoryModal} close={this.closeCategoryModal} />
+      <LoginModal open={this.state.loginModal} close={this.closeLoginModal} />
+      <ComunityList />
+    </div>
+    );
+  }
+    
+}
+
+export default App;
---
+++ meonghamyo-client/src/component/modal/CategoryModal.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import '../css/CategoryModal.css'
+
+class CategoryModal extends Component {
+    render(){
+        const{ open, close } = this.props;
+        return(
+            <div>
+                {open? (
+                <div id='menuModal' className='modal'>
+                    <div className='modalContent'>
+                        <span className='close' onClick={close}>&times;</span>
+                        <ul className='modalData'>
+                            <li className='category'>분양</li>
+                            <li className='category'>커뮤니티</li>
+                        </ul>
+                    </div>
+                </div>
+                ):null}
+            </div>
+        )
+    }
+}
+
+export default CategoryModal;
