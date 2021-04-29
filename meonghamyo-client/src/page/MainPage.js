e601fa06c3939e593c9ab252ba3e42d104c740fa
junhyok <junhyok153@gmail.com>
2021-04-29 20:27:38 +0900

    add MainPage

---
+++ meonghamyo-client/src/page/MainPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+// import "../App.css";
+import Navigation from "../component/Nav";
+import Banner from "../component/Banner";
+import PreSaleList from "../component/PreSaleList";
+import Footer from "../component/Footer";
+
+function MainPage() {
+   return (
+      <div>
+         <header className="App-header">
+            <Navigation></Navigation>
+         </header>
+         <Banner></Banner>
+         <PreSaleList></PreSaleList>
+         <footer>
+            <Footer></Footer>
+         </footer>
+      </div>
+   );
+}
+
+export default MainPage;
+/* abc */
