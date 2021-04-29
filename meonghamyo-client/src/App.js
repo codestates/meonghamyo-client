78e8e5cec19c9706d132397a7cc5f47cddc0e841
junhyok <junhyok153@gmail.com>
2021-04-29 21:51:42 +0900

    finish sprint1

--- meonghamyo-client/src/App.js
+++ meonghamyo-client/src/App.js
@@ -1,57 +1,58 @@
 // import './App.css';
-import './component/css/Nav.css'
-import React, { useState } from 'react';
+import "./component/css/Nav.css";
+import React, { useState } from "react";
 // import { Switch, Route, Redirect, withRouter } from "react-router-dom";
-import ComunityList from './page/ComunityList.js';
-import ContentPage from './page/ContentPage.js'
-import { AiOutlineMenu } from 'react-icons/ai';
-import CategoryModal from './component/CategoryModal';
-import LoginModal from './component/LoginModal'
-import Mypage from './page/Mypage'
-import Signup from './page/Signup'
+import ComunityList from "./page/ComunityList.js";
+import ContentPage from "./page/ContentPage.js";
+import { AiOutlineMenu } from "react-icons/ai";
+import CategoryModal from "./component/CategoryModal";
+import LoginModal from "./component/LoginModal";
+import Mypage from "./page/Mypage";
+import Signup from "./page/Signup";
+import WriteContent from "./page/WriteContent";
+import MainPage from "./page/MainPage";
 
+function App() {
+   const [categoryModal, setCategoryModal] = useState(false);
+   const [loginModal, setLoginModal] = useState(false);
 
-function App(){
-  const [categoryModal, setCategoryModal] = useState(false);
-  const [loginModal, setLoginModal] = useState(false);
-  
-  const openCategoryModal = () => {
-    setCategoryModal(true);
-  }
-  const closeCategoryModal = () => {
-    setCategoryModal(false);
-  }
-  const openLoginModal = () => {
-    setLoginModal(true);
-  }
-  const closeLoginModal = () => {
-    setLoginModal(false);
-  }
+   const openCategoryModal = () => {
+      setCategoryModal(true);
+   };
+   const closeCategoryModal = () => {
+      setCategoryModal(false);
+   };
+   const openLoginModal = () => {
+      setLoginModal(true);
+   };
+   const closeLoginModal = () => {
+      setLoginModal(false);
+   };
 
-    return (
-    <div className="App">
-      <nav>
-          <ul className='navContainer'>
-              <li id='ham' onClick={openCategoryModal}>
+   return (
+      <div className="App">
+         <nav>
+            <ul className="navContainer">
+               <li id="ham" onClick={openCategoryModal}>
                   <AiOutlineMenu />
-              </li>
-              <li id='navLogo'>MeongHaMyo</li>
-              <li id='loginBlock' onClick={openLoginModal}>
+               </li>
+               <li id="navLogo">MeongHaMyo</li>
+               <li id="loginBlock" onClick={openLoginModal}>
                   로그인
-              </li>
-          </ul>
-      </nav>
+               </li>
+            </ul>
+         </nav>
 
-      <CategoryModal open={categoryModal} close={closeCategoryModal} />
-      <LoginModal open={loginModal} close={closeLoginModal} />
-      {/* <ComunityList /> */}
-      {/* <ContentPage /> */}
-      <Mypage />
-      {/* <Signup /> */}
-
-    </div>
-    );
-    
+         <CategoryModal open={categoryModal} close={closeCategoryModal} />
+         <LoginModal open={loginModal} close={closeLoginModal} />
+         {/* <ComunityList /> */}
+         {/* <ContentPage /> */}
+         {/* <Mypage /> */}
+         <MainPage></MainPage>
+         {/* <WriteContent></WriteContent> */}
+         {/* <Signup /> */}
+      </div>
+   );
 }
 
 export default App;
--- meonghamyo-client/src/page/MainPage.js
+++ meonghamyo-client/src/page/MainPage.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-// import "../App.css";
-import Navigation from "../component/Nav";
+
 import Banner from "../component/Banner";
 import PreSaleList from "../component/PreSaleList";
 import Footer from "../component/Footer";
@@ -8,9 +7,6 @@ import Footer from "../component/Footer";
 function MainPage() {
    return (
       <div>
-         <header className="App-header">
-            <Navigation></Navigation>
-         </header>
          <Banner></Banner>
          <PreSaleList></PreSaleList>
          <footer>
