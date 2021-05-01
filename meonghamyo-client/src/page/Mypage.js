546dcff3ca4c9d464858cf0d159846ca401b8cf7
alta <altanis1223@gmail.com>
2021-05-01 22:37:46 +0900

    [Client]홍윤기/fix/mypageSeparate

--- meonghamyo-client/src/page/Mypage.js
+++ meonghamyo-client/src/page/Mypage.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "../component/css/Mypage.css";
-import { AiOutlineComment } from "react-icons/ai";
-import { FaUserAltSlash } from "react-icons/fa";
 import { useHistory } from "react-router-dom";
+import UserInfoTable from "../component/UserInfoTable";
+import UserPostList from "../component/UserPostList";
 
 const Mypage = ({
   fakeUser,
@@ -10,8 +10,9 @@ const Mypage = ({
   fakeComment,
   userLogout,
   closeLoginModal,
+  modifyUserInfo,
+  modifyUserPwd,
 }) => {
-  const [isModify, setisModify] = useState(false);
   const [listCheck, setlistCheck] = useState(true);
   const history = useHistory();
   const handleLogout = () => {
@@ -20,142 +21,6 @@ const Mypage = ({
     history.push("/");
   };
 
-  const userPostList = () => {
-    if (listCheck) {
-      return fakeContent === null ? (
-        <>
-          <h1 className="h1PostComment" style={{ color: "#9a9a9a" }}>
-            아직 작성한 게시물이 없습니다.
-          </h1>
-          <h1 className="h1PostComment" style={{ color: "#9a9a9a" }}>
-            게시물을 작성해보세요 !
-          </h1>
-          <AiOutlineComment
-            className="iconPostComment"
-            style={{ color: "#9a9a9a" }}
-            size="80"
-          />
-        </>
-      ) : (
-        <>
-          <div className="userPostList">
-            <span className="userId">{fakeUser.nickname}</span>
-            <div className="userContent">{fakeContent.title}</div>
-          </div>
-          <hr></hr>
-        </>
-      );
-    } else {
-      return fakeComment === null ? (
-        <>
-          <h1 className="h1PostComment" style={{ color: "#9a9a9a" }}>
-            아직 작성한 댓글이 없습니다.
-          </h1>
-          <h1 className="h1PostComment" style={{ color: "#9a9a9a" }}>
-            댓글을 작성해보세요 !
-          </h1>
-          <AiOutlineComment
-            className="iconPostComment"
-            style={{ color: "#9a9a9a" }}
-            size="80"
-          />
-        </>
-      ) : (
-        <>
-          <div className="userCommentList">
-            <span className="userId">{fakeUser.nickname}</span>
-            <div className="userContent">{fakeComment.content}</div>
-          </div>
-          <hr></hr>
-        </>
-      );
-    }
-  };
-  const userInfoTable = () => {
-    if (isModify) {
-      return fakeUser === null ? (
-        <>
-          <FaUserAltSlash
-            style={{ color: "#9a9a9a", marginLeft: "200" }}
-            size="80"
-          />
-          <div className="userNotFound"> 회원정보가 존재하지 않습니다</div>
-        </>
-      ) : (
-        <>
-          <table className="userInfoTable">
-            <tr>
-              아이디(이메일)
-              <td>{fakeUser.email}</td>
-            </tr>
-            <tr>
-              이름
-              <td>
-                <input className="inputName" type="text" />
-              </td>
-            </tr>
-            <tr>
-              닉네임
-              <td>
-                <input className="inputNickName" type="text" />
-              </td>
-            </tr>
-            <tr>
-              생년월일
-              <td>{fakeUser.birth}</td>
-            </tr>
-          </table>
-        </>
-      );
-    } else {
-      return fakeUser === null ? (
-        <>
-          <FaUserAltSlash
-            style={{ color: "#9a9a9a", marginLeft: "200" }}
-            size="80"
-          />
-          <div className="userNotFound"> 회원정보가 존재하지 않습니다</div>
-        </>
-      ) : (
-        <table className="userInfoTable">
-          <tr>
-            아이디(이메일)
-            <td>{fakeUser.email} </td>
-          </tr>
-          <tr>
-            이름
-            <td>{fakeUser.name} </td>
-          </tr>
-          <tr>
-            닉네임
-            <td>{fakeUser.nickname} </td>
-          </tr>
-          <tr>
-            비밀번호 변경
-            <td>
-              현재 비밀번호
-              <input className="currentPwd" type="password" />
-              <br />
-              <br />
-              새 비밀번호
-              <input className="newPwd" type="password" />
-              <br />
-              <br />
-              비밀번호 다시 입력
-              <input className="newPwdCheck" type="password" />
-              <br />
-              <br />
-              <button className="userPwdModifyBtn">비밀번호 변경</button>
-            </td>
-          </tr>
-          <tr>
-            생년월일
-            <td>{fakeUser.birth}</td>
-          </tr>
-        </table>
-      );
-    }
-  };
   return fakeUser === null ? (
     <div>
       FakeUser가 Null 일 경우 에러 떠서 보기싫어가지고 일단 임시로 이걸 열어
@@ -170,36 +35,11 @@ const Mypage = ({
           <img className="userImage" src={fakeUser.image} />
           <button className="pictureBtn">사진 변경</button>
         </div>
-        {userInfoTable()}
-      </div>
-      <div className="modifyBtnDiv">
-        {isModify ? (
-          <>
-            <button className="userModifyAccessBtn">확인</button>
-            <button
-              className="userModifyCancelBtn"
-              onClick={() => {
-                setisModify(false);
-              }}
-            >
-              취소
-            </button>
-          </>
-        ) : (
-          <>
-            <button
-              className="userModifyBtn"
-              onClick={() => {
-                setisModify(true);
-              }}
-            >
-              회원정보 수정
-            </button>
-            <button className="userLeaveBtn" onClick={() => {}}>
-              회원 탈퇴
-            </button>
-          </>
-        )}
+        <UserInfoTable
+          fakeUser={fakeUser}
+          modifyUserInfo={modifyUserInfo}
+          modifyUserPwd={modifyUserPwd}
+        />
       </div>
       <div className="postList">
         <h1>내가 쓴 글 리스트</h1>
@@ -221,7 +61,12 @@ const Mypage = ({
             댓글
           </button>
         </div>
-        {userPostList()}
+        <UserPostList
+          listCheck={listCheck}
+          fakeContent={fakeContent}
+          fakeComment={fakeComment}
+          fakeUser={fakeUser}
+        />
       </div>
 
       <button className="logoutBtn" onClick={handleLogout}>
